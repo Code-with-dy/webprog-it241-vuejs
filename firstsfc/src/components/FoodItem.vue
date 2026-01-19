1d27042d64fcf4fb81cd60a3f63009590f0cd206
Code-with-dy <ddvillanueva@student.apc.edu.ph>
2026-01-19 15:00:18 +0800

    updated

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Mangoes">
+  <div class="mangoes">
     <h2>{{ name }}</h2>
     <p>{{ message }}</p>
   </div>
@@ -17,11 +17,11 @@ export default {
 </script>
 
 <style>
-  .Mangoes{
+  .mangoes{
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: Yellow ;
+    background-color: yellow ;
   }
 </style>
