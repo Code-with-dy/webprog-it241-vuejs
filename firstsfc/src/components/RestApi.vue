0d313a71c35b3611c832e7c581a15a691b8d945d
Code-with-dy <ddvillanueva@student.apc.edu.ph>
2026-02-06 08:00:38 +0000

    .

--- firstsfc/src/components/RestApi.vue
+++ firstsfc/src/components/RestApi.vue
@@ -16,97 +16,25 @@
 
     <h3>Add New Post</h3>
     <form @submit.prevent="addPost">
-      <input type="text" v-model="newPost.title" placeholder="Title" required><br>
-      <textarea v-model="newPost.body" placeholder="Body" required></textarea><br>
+      <!-- Added self-closing slashes below -->
+      <input type="text" v-model="newPost.title" placeholder="Title" required />
+      <br />
+      <textarea v-model="newPost.body" placeholder="Body" required></textarea>
+      <br />
       <button type="submit">Add Post</button>
     </form>
 
     <div v-if="editingPost">
       <h3>Edit Post</h3>
       <form @submit.prevent="updatePost">
-        <input type="text" v-model="editPostData.title" placeholder="Title" required><br>
-        <textarea v-model="editPostData.body" placeholder="Body" required></textarea><br>
+        <!-- Added self-closing slashes below -->
+        <input type="text" v-model="editPostData.title" placeholder="Title" required />
+        <br />
+        <textarea v-model="editPostData.body" placeholder="Body" required></textarea>
+        <br />
         <button type="submit">Update Post</button>
         <button @click="cancelEdit">Cancel</button>
       </form>
     </div>
   </div>
 </template>
-
-<script setup>
-import { ref, reactive, onMounted } from 'vue';
-import axios from 'axios';
-
-const posts = ref([]);
-const loading = ref(false);
-const error = ref(null);
-const newPost = reactive({ title: '', body: '' });
-const editingPost = ref(false);
-const editPostData = reactive({ title: '', body: '', id: null });
-const selectedPost = ref(null);
-
-const fetchPosts = async () => {
-  loading.value = true;
-  error.value = null;
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    posts.value = response.data;
-  } catch (err) {
-    error.value = err.message;
-  } finally {
-    loading.value = false;
-  }
-};
-
-const addPost = async () => {
-  try {
-    const response = await axios.post('https://jsonplaceholder.typicode.com/posts', newPost);
-    posts.value.push(response.data);
-    newPost.title = '';
-    newPost.body = '';
-  } catch (err) {
-    error.value = err.message;
-  }
-};
-
-const deletePost = async (id) => {
-  try {
-    await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    posts.value = posts.value.filter(post => post.id !== id);
-  } catch (err) {
-    error.value = err.message;
-  }
-};
-
-const editPost = (post) => {
-  editingPost.value = true;
-  editPostData.title = post.title;
-  editPostData.body = post.body;
-  editPostData.id = post.id;
-  selectedPost.value = post;
-};
-
-const updatePost = async () => {
-  try {
-    const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${editPostData.id}`, editPostData);
-    const index = posts.value.findIndex(p => p.id === editPostData.id);
-    if (index !== -1) {
-      posts.value[index] = response.data;
-    }
-    editingPost.value = false;
-    selectedPost.value = null;
-  } catch (err) {
-    error.value = err.message;
-  }
-};
-
-const cancelEdit = () => {
-  editingPost.value = false;
-  editPostData.title = '';
-  editPostData.body = '';
-  editPostData.id = null;
-  selectedPost.value = null;
-};
-
-onMounted(fetchPosts);
-</script>
